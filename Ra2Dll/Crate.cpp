355cc26a5aedf9a7b751cd806536a99217685ed0
BIGSINGER <pushebp@163.com>
2025-07-03 18:25:44 +0800

    1

--- Ra2Dll/Crate.cpp
+++ Ra2Dll/Crate.cpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <MapClass.h>
 #include "Crate.h"
 #include "Utils.h"
@@ -10,19 +10,19 @@ const char* powerupNames[] = {
     "HealBase（基地治疗）",
     "Cloak（隐形）",
     "Explosion（爆炸）",
-    "Napalm（凝固汽油弹）",
+    "Napalm",
     "Squad（小队）",
     "Darkness（黑暗）",
     "Reveal（揭示）",
     "Armor（护甲）",
-    "Speed（速度）",
+    "Speed（加速）",
     "Firepower（火力）",
     "ICBM（洲际弹道导弹）",
     "Invulnerability（无敌）",
-    "Veteran（老兵）",
+    "Veteran（升级）",
     "IonStorm（离子风暴）",
     "Gas（气体）",
-    "Tiberium（泰伯利亚矿）",
+    "Tiberium",
     "Pod（舱体）"
 };
 
