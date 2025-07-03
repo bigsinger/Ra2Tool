b0cc749a0503084df76a4f9551d9ca618e849618
BIGSINGER <pushebp@163.com>
2025-07-03 18:20:09 +0800

    1

--- Ra2Dll/Crate.cpp
+++ Ra2Dll/Crate.cpp
@@ -5,25 +5,25 @@
 
 // 定义一个字符串数组，每个字符串对应一个枚举值
 const char* powerupNames[] = {
-    "Money",
-    "Unit",
-    "HealBase",
-    "Cloak",
-    "Explosion",
-    "Napalm",
-    "Squad",
-    "Darkness",
-    "Reveal",
-    "Armor",
-    "Speed",
-    "Firepower",
-    "ICBM",
-    "Invulnerability",
-    "Veteran",
-    "IonStorm",
-    "Gas",
-    "Tiberium",
-    "Pod"
+    "Money（金钱）",
+    "Unit（单位）",
+    "HealBase（基地治疗）",
+    "Cloak（隐形）",
+    "Explosion（爆炸）",
+    "Napalm（凝固汽油弹）",
+    "Squad（小队）",
+    "Darkness（黑暗）",
+    "Reveal（揭示）",
+    "Armor（护甲）",
+    "Speed（速度）",
+    "Firepower（火力）",
+    "ICBM（洲际弹道导弹）",
+    "Invulnerability（无敌）",
+    "Veteran（老兵）",
+    "IonStorm（离子风暴）",
+    "Gas（气体）",
+    "Tiberium（泰伯利亚矿）",
+    "Pod（舱体）"
 };
 
 const char* getCrateName(byte crateType) {
