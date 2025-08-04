71dbcd87cd1c23b1d42cdfbd2ad7aa9b3c0213a9
BIGSING <pushebp@163.com>
2025-08-04 22:24:38 +0800

    1

--- Ra2Dll/Ra2Helper.cpp
+++ Ra2Dll/Ra2Helper.cpp
@@ -273,10 +273,10 @@ void LevelUpSelectings() {
 // 强制显身，效果：幻影/间谍/隐身状态会被强显
 void DisableDisguise() {
 	Utils::Log("反隐功能");
-	//if (SessionClass::IsMultiplayer()) {
-	//	Utils::Log("反隐功能在多人模式下会平行世界，本次不会开启，推荐在单人模式下使用。");
-	//	return;
-	//}
+	if (SessionClass::IsMultiplayer()) {
+		Utils::Log("反隐功能在多人模式下会平行世界，本次不会开启，推荐在单人模式下使用。");
+		return;
+	}
 
 #if 0
 	for (int i = 0; i < TechnoTypeClass::Array.Count; i++) {
